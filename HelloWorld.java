e8112e95d03c279a7a68f2032870803a50ad83c6
Colton Mecham <cmecham@stu.jsu.edu>
2019-08-31 19:34:41 -0500

    Added f2()

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package helloworld;
 
-/**
- *
- * @author colto
- */
+
 public class HelloWorld {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+Class1 c1 = new Class1();
+Class2 c2 = new Class2();
+System.out.println("Hello, World!");   } 
+        
     }
+	private void f1() {       
+		// Placeholder method (v1.0)   } 
+
+	private void f2() {
+		// Placeholder method (v2.0) } 
+
     
 }
